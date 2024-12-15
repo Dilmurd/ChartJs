7549356283aff395b978edd24e0ef393c9fa0f97
Dilmurd <0nishonov.dilmurod@gmail.com>
2024-12-16 01:05:48 +0500

    fixed bag again

--- src/pages/dashboard/manage-product/ManageProduct.jsx
+++ src/pages/dashboard/manage-product/ManageProduct.jsx
@@ -2,14 +2,14 @@ import Back from '@/components/Back/Back'
 import Today from '@/components/Today/Today'
 import React from 'react'
 import Piechart from '@/components/Pie/Piechart'
-import lineChart from '@/components/Line-chart/Linechart'
+import LineChart from '@/components/Line-chart/Linechart'
 
 const ManageProduct = () => {
   return (
     <>
       <Today/>
       <Back/>
-      <Linechart/>
+      <LineChart/>
       <Piechart/>
     </>
   )
